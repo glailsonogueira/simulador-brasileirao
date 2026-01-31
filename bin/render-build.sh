476ac085a70c63be859dfe9eb9d5b92a37ba3e13
Glailson Lima Nogueira <glailsonogueira@gmail.com>
2026-01-31 22:12:26 +0000

    fix: Forçar seeds a rodar sempre

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,11 +6,4 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
-# Rodar seeds se ainda não tem usuários
-if bundle exec rails runner "exit(User.count == 0 ? 0 : 1)"; then
-  echo "==> Running seeds..."
-  bundle exec rails db:seed
-else
-  echo "==> Seeds already ran, skipping..."
-fi
+bundle exec rails db:seed
